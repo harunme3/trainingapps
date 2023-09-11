d2d84978a8b24004e1d481aa79559773c3aa8998
Rahul Kumar Gupta <rahulguptasonu123@gmail.com>
2023-09-11 15:53:47 +0530

    sec

---
+++ lib/constants/colorconst.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+
+const skprimaryColor = Color(0xFF53B175);
--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:trainingapp/constants/colorconst.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -10,6 +11,7 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(backgroundColor: skprimaryColor,
+    );
   }
 }
